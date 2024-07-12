7b93dbce0e198e636bc19a790c13de39ecd54280
djsilvajr <douglasjr0809@gmail.com>
2024-07-12 01:40:46 -0300

    Criação de projeto + miration de tabela para custumers

---
+++ database/migrations/2024_07_12_041625_create_custumers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustumersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('custumers', function (Blueprint $table) {
+            $table->id()->comment('Chave primeira da tabela de clientes.'); 
+            $table->string('name')->comment('Nome do cliente.');
+            $table->date('birth_date')->comment('data de nascimento do cliente.');
+            $table->string('email')->unique()->comment('Email do cliente.');
+            $table->enum('gender', ['M', 'F'])->comment('Genero do cliente M para masculino e F para feminino.');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('custumers');
+    }
+}
